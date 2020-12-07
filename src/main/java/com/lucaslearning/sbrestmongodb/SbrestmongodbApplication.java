ca33046fdb6ed817db453817d0446c3e5164a302
Lucas Silva <lucas_segundinho@hotmail.com>
2020-12-07 14:48:08 +0000

    first commit - projeto criado

---
+++ src/main/java/com/lucaslearning/sbrestmongodb/SbrestmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.lucaslearning.sbrestmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbrestmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbrestmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucaslearning/sbrestmongodb/SbrestmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaslearning.sbrestmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbrestmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
