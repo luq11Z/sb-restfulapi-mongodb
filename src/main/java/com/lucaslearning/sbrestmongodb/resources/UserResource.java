8b4d943966b47b090343c11ca1aff957c9d74349
Lucas Silva <lucas_segundinho@hotmail.com>
2020-12-07 17:13:37 +0000

    Inserção de usuário com POST

--- src/main/java/com/lucaslearning/sbrestmongodb/resources/UserResource.java
+++ src/main/java/com/lucaslearning/sbrestmongodb/resources/UserResource.java
@@ -1,14 +1,18 @@
 package com.lucaslearning.sbrestmongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucaslearning.sbrestmongodb.domain.User;
 import com.lucaslearning.sbrestmongodb.dto.UserDTO;
@@ -33,5 +37,14 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		//cabecalho com url do novo recurso criado (boas praticas)
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build(); //created retorna o codigo 201 (codigo de resposta http quando um recurso e criado)
 	}
 }
