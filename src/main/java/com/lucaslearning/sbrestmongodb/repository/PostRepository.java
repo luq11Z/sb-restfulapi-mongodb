f90f6bb3a599283b78af6a5e0d4e389937b0a0e0
Lucas Silva <lucas_segundinho@hotmail.com>
2020-12-07 21:27:36 +0000

    Consulta simples com query methods

--- src/main/java/com/lucaslearning/sbrestmongodb/repository/PostRepository.java
+++ src/main/java/com/lucaslearning/sbrestmongodb/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.lucaslearning.sbrestmongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import com.lucaslearning.sbrestmongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
