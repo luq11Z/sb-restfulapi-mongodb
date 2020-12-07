24768d4d23ff2ab279d8fa9588401c99a0ffd041
Lucas Silva <lucas_segundinho@hotmail.com>
2020-12-07 20:10:38 +0000

    Criando entity Post com User aninhado

--- src/main/java/com/lucaslearning/sbrestmongodb/config/Instantiation.java
+++ src/main/java/com/lucaslearning/sbrestmongodb/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.lucaslearning.sbrestmongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.lucaslearning.sbrestmongodb.domain.Post;
 import com.lucaslearning.sbrestmongodb.domain.User;
+import com.lucaslearning.sbrestmongodb.repository.PostRepository;
 import com.lucaslearning.sbrestmongodb.repository.UserRepository;
 
 @Configuration //anotacao necessari para o spring entender que e uma configuracao
@@ -15,17 +19,27 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRep;
 	
+	@Autowired
+	private PostRepository postRep;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRep.deleteAll();
+		postRep.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		userRep.saveAll(Arrays.asList(maria,alex,bob));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
 		
+		userRep.saveAll(Arrays.asList(maria,alex,bob));
+		postRep.saveAll(Arrays.asList(post1,post2));
 	}
 
 }
---
+++ src/main/java/com/lucaslearning/sbrestmongodb/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.lucaslearning.sbrestmongodb.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucaslearning.sbrestmongodb.domain.Post;
+
+//a partir disso e possivel realizar operacoes em base de dados para essa classe
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+}
