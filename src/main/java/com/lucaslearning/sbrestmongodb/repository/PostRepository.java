acced0a5271653577822d20f47863f93fda53b77
Lucas Silva <lucas_segundinho@hotmail.com>
2020-12-07 21:39:01 +0000

    Consulta simples com @Query

--- src/main/java/com/lucaslearning/sbrestmongodb/repository/PostRepository.java
+++ src/main/java/com/lucaslearning/sbrestmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.lucaslearning.sbrestmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lucaslearning.sbrestmongodb.domain.Post;
@@ -11,5 +12,9 @@ import com.lucaslearning.sbrestmongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
-	List<Post> findByTitleContainingIgnoreCase(String text);
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") //query personalizado (dados no link do material de apoio)
+	List<Post> searchTitle(String text);
+	
+	List<Post> findByTitleContainingIgnoreCase(String text); //metodo de pesquisa ja encorporado no mongodb (verificar material de apoio)
+	
 }
--- src/main/java/com/lucaslearning/sbrestmongodb/services/PostService.java
+++ src/main/java/com/lucaslearning/sbrestmongodb/services/PostService.java
@@ -22,7 +22,7 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text) {
-		return rep.findByTitleContainingIgnoreCase(text);
+		return rep.searchTitle(text);
 	}
 	
 }
