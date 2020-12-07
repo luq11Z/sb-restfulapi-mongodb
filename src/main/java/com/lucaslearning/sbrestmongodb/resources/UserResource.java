fec1307490ff5ad6e9eeb2c4d3c56c0d75de3090
Lucas Silva <lucas_segundinho@hotmail.com>
2020-12-07 16:01:11 +0000

    Usando padrão DTO para retornar usuários

--- src/main/java/com/lucaslearning/sbrestmongodb/resources/UserResource.java
+++ src/main/java/com/lucaslearning/sbrestmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.lucaslearning.sbrestmongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucaslearning.sbrestmongodb.domain.User;
+import com.lucaslearning.sbrestmongodb.dto.UserDTO;
 import com.lucaslearning.sbrestmongodb.services.UserService;
 
 @RestController
@@ -19,8 +21,10 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		//para cada obj user retornar um novo user dto passando o x como argumento e depois voltar o stream para uma lista
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
